433a45fd3ee5e7f6d3c23fbc38c57b27240c884f
unsiqasik <rkhandrianto17@gmail.com>
2024-10-12 19:31:48 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print ("Hello Ser")
+print ("Saya Muhammad Rakha Qushayyi Andrianto")
+print ("Saya belajar Penjumlahan dengan Python")
+
+a=int(input("Masukan Angka"))
+b=int(input("Masukan Angka"))
+
+tambah=a+b
+kali=a*b
+kurang=a-b
+bagi=a/b
+print("Hasil Penjumlahan", tambah)
+print("Hasil Pengurangan", kurang)
+print("Hasil Perkalian", kali)
+print("Hasil Pembagian", bagi)
+
+print("Sekian Terimakasih")
